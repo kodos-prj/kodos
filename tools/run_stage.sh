f95247f73894e9cc9acd0186003daef6cba4b99e
Antal Buss <antal.buss@gmail.com>
2024-10-12 00:49:07 -0600

    Added tool to run post_{install/update} functions

---
+++ tools/run_stage.sh
@@ -0,0 +1,21 @@
+#!bash
+
+SCRIPT=$1
+STAGE=$2
+
+if [ -e $SCRIPT ]; then
+  source $SCRIPT
+else
+  echo "File $SCRIPT doesn't existing"
+  exit
+fi
+
+case $STAGE in
+  "post_install" ) 
+      echo "Running $SCRIPT post_install stage";
+      post_install ;;
+  "post_update" ) 
+      echo "Running $SCRIPT post_update stage";
+      post_update ;;
+esac
+
