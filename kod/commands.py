0e264a8294ad4829c77f372f2370470fe85606a9
Antal Buss <antal.buss@gmail.com>
2024-12-20 16:51:40 -0700

    One more extra parameter

--- kod/commands.py
+++ kod/commands.py
@@ -801,7 +801,7 @@ def rebuild(c, config, new_generation=False):
     # pkg_list = list(conf.packages.values())
     pkg_list, rm_pkg_list = get_packages_to_install(c, conf)
     pkg_list += proc_hardware(c, conf)
-    service_list, service_to_enable = proc_services(c, conf, repos)
+    service_list, service_to_enable = proc_services(c, conf)
     pkg_list += service_list
 
     print("packages\n",pkg_list)
