cbf9eba66fa29804da5c3ed284b08dd69aaf4911
Antal Buss <antal.buss@gmail.com>
2024-12-27 02:49:20 -0700

    Added some degub prints

--- kod/commands.py
+++ kod/commands.py
@@ -433,10 +433,12 @@ def manage_packages(c, root_path, repos, action, list_of_packages, chroot=False)
         if len(pkgs) == 0:
             continue
         if "run_as_root" in repos[repo] and not repos[repo]["run_as_root"]:
+            print("[DEBUG-1]",f"{exec_prefix} runuser -u kod -- {repos[repo][action]} {' '.join(pkgs)}")
             c.run(
                 f"{exec_prefix} runuser -u kod -- {repos[repo][action]} {' '.join(pkgs)}"
             )
         else:
+            print("[DEBUG-2]", f"{exec_prefix} {repos[repo][action]} {' '.join(pkgs)}")
             c.run(f"{exec_prefix} {repos[repo][action]} {' '.join(pkgs)}")
         packages_installed += pkgs
     return packages_installed
@@ -1096,7 +1098,7 @@ def rebuild(c, config, new_generation=False, update=False):
 
     if added_pkgs:
         print("Packages to install:", added_pkgs)
-        manage_packages(c, root_path, repos, "update", added_pkgs, chroot=use_chroot)
+        manage_packages(c, root_path, repos, "install", added_pkgs, chroot=use_chroot)
 
     # System services
     print(f"Services to enable: {new_service_to_enable}")
