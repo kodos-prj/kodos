e7a5d47d53ee9c821fba2af908be990bc99dfb8d
Antal Buss <antal.buss@gmail.com>
2025-10-06 01:12:23 -0600

    Added mirror list generation

--- install_kodos.sh
+++ install_kodos.sh
@@ -1,7 +1,11 @@
 #!/bin/bash
 
 echo "Installing required packages"
+pacman -Syy
 pacman -S git uv whois --noconfirm
+
+echo "Update mirrorlist"
+curl 'https://archlinux.org/mirrorlist/?country=CA&protocol=http&protocol=https&ip_version=4' -o /etc/pacman.d/mirrorlist
 
 echo "Cloning kodos repo"
 git clone https://github.com/kodos-prj/kodos
